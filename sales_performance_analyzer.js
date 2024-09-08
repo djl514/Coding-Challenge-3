e82c96254d5686e91550daf2586b8888d7600650
David Layne <davidlayne@usf.edu>
2024-09-08 18:13:57 -0400

    created calculateTotalSales function to reduce redundancy

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -7,27 +7,35 @@ const salesData = [
 ];
 console.log(salesData[1].sales);
 
-function calculateAverageSales(num){
+function calculateTotalSales(num){
     let totalSales = salesData[num].sales.reduce((total, sales) => total + sales, 0);
-    return totalSales/salesData[num].sales.length;
+    return totalSales;
 }
 
-console.log(calculateAverageSales(3));
+function calculateAverageSales(num){
+    return calculateTotalSales(num)/salesData[num].sales.length;
+}
+
+console.log(calculateAverageSales(0));
 
 //2. Create a Function to determine performance rating
 
-function determinePerformanceRating(num){
-    if (calculateAverageSales(num) > 10000){
-        console.log(`Excellent!`);
-    } else if (calculateAverageSales(num) > 7000 
-    && calculateAverageSales(num) < 10000){
-        console.log('Good!');
-    } else if (calculateAverageSales(num) > 4000 
-    && calculateAverageSales(num) < 7000){
-        console.log('Satisfactory');
+function determinePerformanceRating(num1){
+    if (calculateAverageSales(num1) > 10000){
+        return console.log(`Excellent!`);
+    } else if (calculateAverageSales(num1) > 7000 
+    && calculateAverageSales(num1) < 10000){
+        return console.log('Good!');
+    } else if (calculateAverageSales(num1) > 4000 
+    && calculateAverageSales(num1) < 7000){
+        return console.log('Satisfactory');
     } else {
-        console.log('Needs Improvement');
+        return console.log('Needs Improvement');
     }
 }
 
-console.log(determinePerformanceRating(3));
+console.log(determinePerformanceRating(0));
+
+//3. Create a function to identify top and bottom performers
+
+// Had to tank this, unable to fully complete the final tasks.
