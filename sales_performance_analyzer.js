251ad4088c3e4d9d76a3a9188831986e251d7fae
David Layne <davidlayne@usf.edu>
2024-09-08 11:04:47 -0400

    Create determinePerformanceRating function - using salesData

--- sales_performance_analyzer.js
+++ sales_performance_analyzer.js
@@ -8,8 +8,26 @@ const salesData = [
 console.log(salesData[1].sales);
 
 function calculateAverageSales(num){
-    let totalSales = salesData[num].sales.reduce((total, sales) => total + sales);
+    let totalSales = salesData[num].sales.reduce((total, sales) => total + sales, 0);
     return totalSales/salesData[num].sales.length;
 }
 
-console.log(calculateAverageSales(0));
+console.log(calculateAverageSales(3));
+
+//2. Create a Function to determine performance rating
+
+function determinePerformanceRating(num){
+    if (calculateAverageSales(num) > 10000){
+        console.log(`Excellent!`);
+    } else if (calculateAverageSales(num) > 7000 
+    && calculateAverageSales(num) < 10000){
+        console.log('Good!');
+    } else if (calculateAverageSales(num) > 4000 
+    && calculateAverageSales(num) < 7000){
+        console.log('Satisfactory');
+    } else {
+        console.log('Needs Improvement');
+    }
+}
+
+console.log(determinePerformanceRating(3));
